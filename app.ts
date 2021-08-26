2a13d2a2e21dcf2bc2274ca7f8017499506159c7
Eduardo Oviedo <eduardo.oviedo@gmail.com>
2021-08-26 02:59:07 -0600

    code added

--- app.ts
+++ app.ts
@@ -1,12 +1,25 @@
+interface IEngine {
+    horsePower: number;
+    run: Function;
+}
+
+interface IGasEngine extends IEngine {
+    cc: number;
+}
+
+interface IElectricEngine extends IEngine {
+    voltage: string;
+}
+
 class Car {
     id: number;
     brand: string;
-    engine: Engine;
+    engine: IEngine;
 
-    constructor(id: number, brand: string, horsePower: number, cc: number) {
+    constructor(id: number, brand: string, engine: IEngine) {
         this.id = id;
         this.brand = brand;
-        this.engine = new Engine(horsePower, cc);
+        this.engine = engine;
     }
 
     ride() {
@@ -15,7 +28,7 @@ class Car {
     }
 }
 
-class Engine {
+class GasEngine implements IGasEngine {
     horsePower: number;
     cc: number;
     constructor(horsePower: number, cc: number) {
@@ -28,5 +41,23 @@ class Engine {
     }
 }
 
-const lamborghini = new Car(123, 'Lamborghini', 769, 6498);
+class ElectricEngine implements IElectricEngine {
+    voltage: string;
+    horsePower: number;
+
+    constructor(horsePower: number, voltage: string) {
+        this.horsePower = horsePower;
+        this.voltage = voltage;
+    }
+
+    run() {
+        console.info(`Running engine of ${this.voltage} voltage at ${this.horsePower} horse power.`);
+    };
+
+}
+
+const lamborghini = new Car(123, 'Lamborghini', new GasEngine(769, 6498));
 lamborghini.ride();
+
+const tesla = new Car(456, 'Tesla', new ElectricEngine(769, '350V'));
+tesla.ride();
